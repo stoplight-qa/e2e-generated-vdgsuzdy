c8f263c7b5326ebf17efd98063430d46f94b5ce6
qa-asgdm-2-sdny@example.com <qa-asgdm-2-sdny@example.com>
2023-02-01 16:54:16 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15099.stoplight-dev.com/api/v1/projects/cHJqOjMyMg/spectral.js?branch_slug=master&token=3f2f0985-4f2b-43f0-9467-0e76b09a19b4";
+export default { extends: ruleset };
